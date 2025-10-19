5ebf1047451976b429934b2a36e2b029dfd624f2
krasnodar-rp-business <w43131266@gmail.com>
2025-10-19 16:02:37 +0300

    Create config.js

---
+++ config.js
@@ -0,0 +1,10 @@
+// Конфигурация приложения
+const CONFIG = {
+    GIST_CONFIG: {
+        GIST_ID: '9a5d16a92c83a089ef6b3926577e6324',
+        FILENAME: 'krasnodar_business_data.json',
+        GITHUB_TOKEN: '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'
+    },
+    SYNC_INTERVAL: 10000, // Синхронизация каждые 10 секунд
+    ADMIN_PASSWORD: "EexfAgdK"
+};
